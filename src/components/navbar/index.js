393dc1dd035579dc812c082d1a341608f12a8c8a
Arga <argaprataman052@gmail.com>
2024-04-09 08:23:12 +0700

    UI: mobile

---
+++ src/components/navbar/index.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="h-[4rem] bg-[#249ded] items-center flex">
+      <p className="text-[#fff] font-semibold text-2xl ps-6">Tabungan Kita</p>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/components/table/index.js
@@ -0,0 +1,9 @@
+const Table = ( ) => {
+  return(
+    <table className="bg-[#fff] mt-2 w-full rounded-md h-auto flex flex-col items-center justify-center">
+      Table Uang
+    </table>
+  )
+}
+
+export default Table;
